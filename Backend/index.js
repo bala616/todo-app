c028377b31cce39500efde2a638c22a3058530dc
bala616 <balajianand041@gmail.com>
2026-04-15 19:12:39 +0530

    todo

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,4 @@
-
+const cors = require("cors");
 
 const express = require("express");
 const mongoose = require("mongoose");
@@ -7,6 +7,8 @@ const Todo = require("./model/Todo");
 
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 mongoose
     .connect("mongodb://localhost:27017/todolistapp")
@@ -14,15 +16,25 @@ mongoose
     .catch((e) => { console.log(e) });
 
 
-app.get("/todolist", (req, res) => {
-
+// get all todos
+app.get("/get", async (req, res) => {
+    const todos = await Todo.find();
+    res.json(todos);
 });
 
+// add todo
+app.post("/add", async (req, res) => {
+    const newTodo = new Todo({ task: req.body.task });
+    const saved = await newTodo.save();
+    res.json(saved);
+});
 
-app.post("todolist", (req, res) => { });
+// delete todo
+app.delete("/delete/:id", async (req, res) => {
+    await Todo.findByIdAndDelete(req.params.id);
+    res.json("Deleted");
+});
 
-
-app.put("/todolist", (req, res) => { });
-
-
-app.delete("/todolist", (req, res) => { });
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
